5aa3ab2446f4a0f43d9f9c3129cad5cb42226fb5
MattiaFezzardi <mattiafezzardi1993@gmail.com>
2023-06-14 16:58:20 +0200

    feat: users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 32);
+            $table->string('name')->nullable();
+            $table->string('surname')->nullable();
+            $table->string('birth_date')->nullable()->date();
             $table->rememberToken();
             $table->timestamps();
         });
