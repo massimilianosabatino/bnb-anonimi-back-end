980d8f2c2091ea54d6d2e07715f07e37261b64a4
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-06-14 17:26:16 +0200

    feat/ services migration

--- database/migrations/2023_06_14_152454_create_services_table.php
+++ database/migrations/2023_06_14_152454_create_services_table.php
@@ -13,11 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('sponsorships', function (Blueprint $table) {
+        Schema::create('services', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->unsignedDecimal('price',4,2);
-            $table->unsignedSmallInteger('time');
             $table->timestamps();
         });
     }
@@ -29,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('sponsorships');
+        Schema::dropIfExists('services');
     }
 };
