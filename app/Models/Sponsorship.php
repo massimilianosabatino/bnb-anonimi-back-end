c74d3a2f6b10543c3a60ec11a0897aca095bc94b
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-06-14 18:41:51 +0200

    add: model

---
+++ app/Models/Sponsorship.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sponsorship extends Model
+{
+    use HasFactory;
+
+    public function apartments()
+    {
+        return $this->belongsToMany(Apartment::class);
+    }
+}
