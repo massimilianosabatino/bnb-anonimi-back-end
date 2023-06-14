fc2e2ad9c4da6c4b1e713b2b87ed6425206c70bf
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-06-14 18:34:55 +0200

    feat/ Apartment model con le relazioni tra tabelle

---
+++ app/Models/Apartment.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Apartment extends Model
+{
+    use HasFactory;
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    
+    public function messages(){
+        return $this->hasMany(Message::class);
+    }
+
+    public function galleries(){
+        return $this->hasMany(Gallery::class);
+    }
+
+    public function views(){
+        return $this->hasMany(View::class);
+    }
+
+    public function services(){
+        return $this->belongsToMany(Service::class);
+    }
+
+    public function sponsorships(){
+        return $this->belongsToMany(Sponsorship::class);
+    }
+}
