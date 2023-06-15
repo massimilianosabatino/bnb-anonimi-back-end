d6280947235dd073d6b7e628479a2b21b549e68b
MattiaFezzardi <mattiafezzardi1993@gmail.com>
2023-06-15 19:04:26 +0200

    user->id tirato fuori con la facade Auth

--- app/Http/Controllers/RegisteredUser/ApartmentController.php
+++ app/Http/Controllers/RegisteredUser/ApartmentController.php
@@ -7,6 +7,9 @@ use App\Models\Apartment;
 use App\Http\Requests\StoreApartmentRequest;
 use App\Http\Requests\UpdateApartmentRequest;
 use Illuminate\Support\Str;
+use App\Model\User;
+use App\Models\User as ModelsUser;
+use Illuminate\Support\Facades\Auth;
 
 class ApartmentController extends Controller
 {
@@ -42,14 +45,13 @@ class ApartmentController extends Controller
 
         $newApartments = new Apartment();
         $newApartments->fill($data);
-
-        //Sto passando uno in user_id perche' bisogna passargli l'id di chi carica ma per ora testiamo
-        $newApartments->user_id = 1;
+        $newApartments->user_id = Auth::id();
         $newApartments->slug = Str::slug($data['title']);
-        //dd($data);
-        // come per lo user id ci sara' da cambiare
+        
+
+        //  ci sara' da cambiare per tomtom
         $newApartments->latitude = 40.12345;
-        $newApartments->longitude =40.12345;
+        $newApartments->longitude = 40.12345;
         $newApartments->save();
 
         return redirect()->route('user.apartment.index')->with('message', 'Appartamento creato con successo!!!');
