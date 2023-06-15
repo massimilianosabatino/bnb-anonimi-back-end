05c880b33302b2d709669ad2518113acb4f34270
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-06-15 11:19:45 +0200

    add: for loop in messages seed

--- database/seeders/MessageSeeder.php
+++ database/seeders/MessageSeeder.php
@@ -17,19 +17,21 @@ class MessageSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        $newMessage = new Message();
-
-        //Get apartment from db and attach to message
-        $apartment = Apartment::inRandomOrder()->first();
-        
-        $newMessage->apartment_id = $apartment->id;
-
-        $newMessage->name = $faker->firstName() . ' ' . $faker->lastName();
-        $newMessage->email = $faker->email();
-        $newMessage->content = $faker->realText();
-        $newMessage->send_date = $faker->date();
-        $newMessage->read = $faker->numberBetween(0,1);
-        
-        $newMessage->save();
+        for ($i=0; $i < 20; $i++) { 
+            $newMessage = new Message();
+    
+            //Get apartment from db and attach to message
+            $apartment = Apartment::inRandomOrder()->first();
+            
+            $newMessage->apartment_id = $apartment->id;
+    
+            $newMessage->name = $faker->firstName() . ' ' . $faker->lastName();
+            $newMessage->email = $faker->email();
+            $newMessage->content = $faker->realText();
+            $newMessage->send_date = $faker->date();
+            $newMessage->read = $faker->numberBetween(0,1);
+            
+            $newMessage->save();
+        }
     }
 }
