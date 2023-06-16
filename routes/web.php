d977cd2b4fb11e124ed8b4f0f6721fb59354cd10
LeoSallu <L.sallustio99@gmail.com>
2023-06-16 10:01:28 +0200

    hotfix

--- routes/web.php
+++ routes/web.php
@@ -25,9 +25,14 @@ Route::get('/dashboard', function () {
 
     Route::middleware('auth')->prefix('user')->name('user.')->group(function () {
     Route::resource('apartment', ApartmentController::class);
+});
+
+Route::get('/dashboard', function () {
+    return view('admin.dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+
 
 require __DIR__.'/auth.php';
