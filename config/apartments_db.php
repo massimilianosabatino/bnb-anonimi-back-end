62789296af5427ec94f4e48bdd4cc44c14a51531
albertogioia93 <albertogioia@hotmail.it>
2023-06-19 12:42:28 +0200

    feat: apartments_db

---
+++ config/apartments_db.php
@@ -0,0 +1,244 @@
+<?php
+return[
+    [
+        'title'=>'Bilocale di periferia',
+        'rooms'=>3,
+        'bathrooms'=>1,
+        'beds'=>2,
+        'square_meters'=>40, //Minimo 28
+        'cover_image'=>'https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
+        'address'=>'Via Saorgio, 7, Torino',
+        'visible'=>'0 - 1',
+        'price'=>20 //Minimo 20
+
+    ],
+    [
+        'title'=>'Trilocale di Borgo Vittoria',
+        'rooms'=>4,
+        'bathrooms'=>1,
+        'beds'=>3,
+        'square_meters'=>65,
+        'cover_image'=>'https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
+        'address'=>'Corso Grosseto, 139, Torino',
+        'visible'=>'0 - 1',
+        'price'=>40
+
+    ],
+    [
+        'title'=>'Bilocale zona Borgo Vittoria',
+        'rooms'=>3,
+        'bathrooms'=>1,
+        'beds'=>1,
+        'square_meters'=>30,
+        'cover_image'=>'https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
+        'address'=>'Via Saorgio, 49, Torino',
+        'visible'=>'0 - 1',
+        'price'=>20
+
+    ],
+    [
+        'title'=>'Appartamento zona Lucento',
+        'rooms'=>3,
+        'bathrooms'=>1,
+        'beds'=>1,
+        'square_meters'=>50,
+        'cover_image'=>'https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
+        'address'=>'Via Orbetello, 85, Torino',
+        'visible'=>'0 - 1',
+        'price'=>40
+
+    ],
+    [
+        'title'=>'Bilocale zona Lucento',
+        'rooms'=>3,
+        'bathrooms'=>1,
+        'beds'=>1,
+        'square_meters'=>45,
+        'cover_image'=>'https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
+        'address'=>'Corso Lucio Quinzio Cincinnato, 222, Torino',
+        'visible'=>'0 - 1',
+        'price'=>30
+
+    ],
+    [
+        'title'=>'Casa Vercelli',
+        'rooms'=>4,
+        'bathrooms'=>2,
+        'beds'=>4,
+        'square_meters'=>100,
+        'cover_image'=>'https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
+        'address'=>'Corso Vercelli, 15, Torino',
+        'visible'=>'0 - 1',
+        'price'=>40
+
+    ],
+    [
+        'title'=>'Casa scuola',
+        'rooms'=>4,
+        'bathrooms'=>1,
+        'beds'=>2,
+        'square_meters'=>40,
+        'cover_image'=>'https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
+        'address'=>'Via Amilcare Ponchielli, 56, Torino',
+        'visible'=>'0 - 1',
+        'price'=>60
+
+    ],
+    [
+        'title'=>'Casa in centro',
+        'rooms'=>6,
+        'bathrooms'=>2,
+        'beds'=>8,
+        'square_meters'=>90,
+        'cover_image'=>'https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
+        'address'=>'Via Po, 15, Torino',
+        'visible'=>'0 - 1',
+        'price'=>90
+
+    ],
+    [
+        'title'=>'Villa del Mare Ligure',
+        'rooms'=>6,
+        'bathrooms'=>2,
+        'beds'=> 4,
+        'square_meters'=>100,
+        'cover_image'=>'https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
+        'address'=>'Via Ronchi, 18, Imperia',
+        'visible'=>'0 - 1',
+        'price'=>60
+
+    ],
+    [
+        'title'=>'Casa di Mirko',
+        'rooms'=>3,
+        'bathrooms'=>1,
+        'beds'=>1,
+        'square_meters'=>60,
+        'cover_image'=>'https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
+        'address'=>'Via Alfredo Fiorini, 89, Roma',
+        'visible'=>'0 - 1',
+        'price'=>40
+
+    ],
+    [
+        'title'=>'Casa del burino',
+        'rooms'=>3,
+        'bathrooms'=>1,
+        'beds'=>1,
+        'square_meters'=>40,
+        'cover_image'=>'https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
+        'address'=>'Via Alessandro Cruto, 1, Roma',
+        'visible'=>'0 - 1',
+        'price'=>40
+
+    ],
+    [
+        'title'=>'Locanda dell agnello macellato',
+        'rooms'=>5,
+        'bathrooms'=>1,
+        'beds'=>3,
+        'square_meters'=>80,
+        'cover_image'=>'https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
+        'address'=>'Viale Carmelo Bene, 253, Roma',
+        'visible'=>'0 - 1',
+        'price'=>80
+
+    ],
+    [
+        'title'=>'Locanda del Puledro Impennato',
+        'rooms'=>10,
+        'bathrooms'=>3,
+        'beds'=>10,
+        'square_meters'=>150,
+        'cover_image'=>'https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
+        'address'=>'Via delle Fornaci, 425, Roma',
+        'visible'=>'0 - 1',
+        'price'=>100
+
+    ],
+    [
+        'title'=>'Casata Corvonero',
+        'rooms'=>5,
+        'bathrooms'=>2,
+        'beds'=>4,
+        'square_meters'=>74,
+        'cover_image'=>'https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
+        'address'=>'Via Maragliano, 70, Firenze',
+        'visible'=>'0 - 1',
+        'price'=>56
+
+    ],
+    [
+        'title'=>'Casata Serpeverde',
+        'rooms'=>5,
+        'bathrooms'=>2,
+        'beds'=>4,
+        'square_meters'=>74,
+        'cover_image'=>'https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
+        'address'=>'Via Maragliano, 80, Firenze',
+        'visible'=>'0 - 1',
+        'price'=>61
+
+    ],
+    [
+        'title'=>'Casa del brindisino',
+        'rooms'=>3,
+        'bathrooms'=>1,
+        'beds'=>2,
+        'square_meters'=>40,
+        'cover_image'=>'https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
+        'address'=>'Via Palestro, 36, Brindisi',
+        'visible'=>'0 - 1',
+        'price'=>36
+
+    ],
+    [
+        'title'=>'Villa Graziosa Taormina',
+        'rooms'=>7,
+        'bathrooms'=>2,
+        'beds'=>6,
+        'square_meters'=>110,
+        'cover_image'=>'https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
+        'address'=>'Via Vincenzo Bellini, 23, Taormina',
+        'visible'=>'0 - 1',
+        'price'=>83
+
+    ],
+    [
+        'title'=>'Casa della nonna',
+        'rooms'=>4,
+        'bathrooms'=>1,
+        'beds'=>6,
+        'square_meters'=>70,
+        'cover_image'=>'https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
+        'address'=>'Via Latiano, 90, Oria',
+        'visible'=>'0 - 1',
+        'price'=>38
+
+    ],
+    [
+        'title'=>'Los Pollos Hermanos',
+        'rooms'=>4,
+        'bathrooms'=>1,
+        'beds'=>2,
+        'square_meters'=>55,
+        'cover_image'=>'https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
+        'address'=>'Piazza Nolana, 13, Napoli',
+        'visible'=>'0 - 1',
+        'price'=>43
+
+    ],
+    [
+        'title'=>'Monolocale Da Boe',
+        'rooms'=>2,
+        'bathrooms'=>1,
+        'beds'=>1,
+        'square_meters'=>30,
+        'cover_image'=>'https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
+        'address'=>'Via Saorgio, 9, Torino',
+        'visible'=>'0 - 1',
+        'price'=>29
+
+    ],
+    
+];
