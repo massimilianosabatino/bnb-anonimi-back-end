241352ebe0cfc03533050eadfd1b1e5e31317cbd
MattiaFezzardi <mattiafezzardi1993@gmail.com>
2023-06-19 18:10:16 +0200

    aggiunti services

--- resources/views/user/apartments/show.blade.php
+++ resources/views/user/apartments/show.blade.php
@@ -7,12 +7,13 @@
                 <h2>{{ $apartment->title }}</h2>
 
 
-                 {{-- visibilita' --}}
-                 <p>Il tuo appartamento e' impostato su: @if ($apartment->visible == true)
-                    'VISIBILE'
-                @elseif ($apartment->visible == false)
-                    'NON VISIBILE'
-                @endif </p>
+                {{-- visibilita' --}}
+                <p>Il tuo appartamento e' impostato su: @if ($apartment->visible == true)
+                        'VISIBILE'
+                    @elseif ($apartment->visible == false)
+                        'NON VISIBILE'
+                    @endif
+                </p>
 
                 @if ($apartment->cover_image)
                     <div class="d-flex justify-content-start show-image">
@@ -30,23 +31,44 @@
                     <li class="list-group-item">Price: {{ $apartment->price }}</li>
                 </ul>
 
-               
+
 
                 {{-- bottone per tornare all'index  --}}
-                <a href="{{ route('user.apartment.index', $apartment) }}" role="button"
-                    class="btn btn-success">Torna ai tuoi appartamenti</a>
+                <a href="{{ route('user.apartment.index', $apartment) }}" role="button" class="btn btn-success">Torna ai
+                    tuoi appartamenti</a>
 
                 {{-- bottone per edit --}}
-                <a href="{{ route('user.apartment.edit', $apartment) }}" role="button"
-                    class="btn btn-warning">Modifica questo appartamento</a>
+                <a href="{{ route('user.apartment.edit', $apartment) }}" role="button" class="btn btn-warning">Modifica
+                    questo appartamento</a>
 
             </div>
 
-                {{-- sponsorizzazioni --}}
+
             <div class="col-3 gy-4">
-                <p class="text-center">Vuoi sponsorizzare il tuo appartamento?</p>
+
+                {{-- vai a messaggi appartamento --}}
+                <p class="text-center sponsor">Domande dai clienti</p>
                 <a href="{{ route('user.apartment.index', $apartment) }}" role="button"
-                    class="btn btn-success w-100">Sponsorizza!</a>
+                    class="btn btn-success w-100">Messaggi ricevuti</a>
+
+                {{-- visualizza le statistiche --}}
+                <p class="text-center sponsor">Visualizza le statistiche</p>
+                <a href="{{ route('user.apartment.index', $apartment) }}" role="button"
+                    class="btn btn-success w-100">STATS !</a>
+
+                {{-- sponsorizzazioni --}}
+                <p class="text-center sponsor">Vuoi sponsorizzare il tuo appartamento?</p>
+                <a href="{{ route('user.apartment.index', $apartment) }}" role="button"
+                    class="btn btn-info w-100">Sponsorizza!</a>
+
+                <div class="apart-services">
+                    @foreach ($apartment->services as $service)
+                        <input type="checkbox" class="btn-check" id="{{ $service->id }}" checked>
+                        <label
+                            class="btn btn-outline-dark d-flex p-1 m-0 justify-content-center align-items-center rounded-2 flex-column"
+                            for="{{ $service->id }}">{!! $service->icon !!}{{ $service->name }}</label><br>
+                    @endforeach
+                </div>
             </div>
 
 
