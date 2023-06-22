2a142f29bbff449a1cb7f4d6304699548ddef92e
Luca Zanfrisco <lzanfrisco9@gmail.com>
2023-06-22 10:01:22 +0200

    Fix risposta api

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -44,25 +44,33 @@ class ApartmentController extends Controller
     }
     public function search(Request $request)
     {
-        $latA = $request['lat'] * 3.14 / 180;
-        $lonA = $request['lon'] * 3.14 / 180;
+        $latA = $request['lat'] * 3.14 / 180; //Conversione in radianti della lat della ricerca
+        $lonA = $request['lon'] * 3.14 / 180; //Conversione in radianti della lon della ricerca
         $dist = $request['dist'];
-        $R = 6372.795477598;
+        $R = 6372.795477598; //Raggio dell'equatore
         $apartments = Apartment::all();
 
         $filtered_apartment=[];
         foreach($apartments as $apartment){
             
-            $latB = $apartment->latitude * 3.14 / 180;
-            $lonB = $apartment->longitude * 3.14 / 180;
-            $calc_dist = $R * acos((sin($latA)* sin($latB) + cos($latA) * cos($latB) * cos($lonA-$lonB)));
+            $latB = $apartment->latitude * 3.14 / 180; // Conversione in radianti della lat dell'appartamento
+            $lonB = $apartment->longitude * 3.14 / 180; //Conversione in radianti della lon dell'appartamento 
+            $calc_dist = $R * acos((sin($latA)* sin($latB) + cos($latA) * cos($latB) * cos($lonA-$lonB))); //Formula trigonometrica per il calcolo della distanza 
             if ($calc_dist < $dist) {
                 array_push($filtered_apartment,$apartment);
             }
         }
+    if(count($filtered_apartment) > 0 ){
         return response()->json([
             'success' => true,
             'results' => $filtered_apartment
         ]);
+    }else{
+        return response()->json([
+            'success' => true,
+            'results' => null
+        ]);
+    }
+        
     }
 }
