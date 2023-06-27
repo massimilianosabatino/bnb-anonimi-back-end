a5a41ac2206614f23494b45cbea0d5d431a5cc89
LeoSallu <L.sallustio99@gmail.com>
2023-06-27 15:26:06 +0200

    feat:view-api

--- app/Http/Controllers/Api/ViewController.php
+++ app/Http/Controllers/Api/ViewController.php
@@ -9,10 +9,10 @@ use Illuminate\Http\Request;
 class ViewController extends Controller
 {
     public function views(View $view,Request $request){
-        
-        $ip_address=$request->ip();
+
         $id_ap=$request['apartment_id'];
         $visit_date=$request['visit_date'];
+        $ip_address=$request['ip_address'];
         
         $view->apartment_id=$id_ap;
         $view->visit_date=$visit_date;
