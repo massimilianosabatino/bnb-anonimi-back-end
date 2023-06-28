c84cb70a942a2b69b798c5b7a43ac95c7a961566
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-06-28 21:00:11 +0200

    change: dashboard - first section

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -18,13 +18,172 @@
     @endif
     {{-- Welcome User --}}
     <div class="container p-4">
-        <h2 class="d-none d-lg-block">Ciao {{ Auth::user()->name }}</h2>
-        <div class="rounded-2 border bg-white shadow p-4 my-4">
+        <div class="row">
+            <div class="col">
+                <h4 class="d-none d-lg-block">Ciao {{ Auth::user()->name }}</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <h1 class="text-center my-4">Carica e gestisci i tuoi appartamenti, sponsorizzali e massimizza i tuoi profitti</h1>
+                <p class="text-center fw-bold my-4">Da questa sezione hai il pieno controllo sui tuoi appartamenti, gestisci semplicemente le tue strutture e migliora le performance con le sponsorizzazioni</h3>
+            </div>
+        </div>
+        <div class="row align-items-center">
+            <div class="col-6">
+                <img class="img-fluid" src="{{ asset('img/front.png') }}" alt="Anteprima front-end">
+            </div>
+            <div class="col-6">
+                <div class="side-img-text">
+                    <div class="fw-bold">
+                        Carica un nuovo appartamento
+                    </div>
+                    <p>Inserisci i dettagli del tuo appartamento e caricalo sulla nostra piattaforma.</p>
+                </div>
+                <div class="side-img-text">
+                    <div class="fw-bold">
+                        Modifica i tuoi appartamenti
+                    </div>
+                    <p>La nostra piattaforma ti permette di modificare in qualsiasi momento tutti i dettagli del tuo appartamento.</p>
+                </div>
+                <div class="side-img-text">
+                    <div class="fw-bold">
+                        Sponsorizza gli appartamenti
+                    </div>
+                    <p>Puoi sponsorizzare i tuoi appartamenti per mostrarli in homepage e per farli salire nei risultati di ricerca degli utenti. Un incentivo per massimizzare i tuoi guadagni.</p>
+                </div>
+                <div class="side-img-text">
+                    <div class="fw-bold">
+                        Messaggi ricevuti
+                    </div>
+                    <p>Puoi visualizzare tutti i messaggi ricevuti per ogni appartamento. I messaggi sono privati, solo tu puoi vederli! Il resto della conversazione può avvenire privatamente, per garantire la massima privacy.</p>
+                </div>
+                <div class="side-img-text">
+                    <div class="fw-bold">
+                        Statistiche personali
+                    </div>
+                    <p>Controlla il risultati raggiunti da i tuoi appartamenti, scopri quelli più richiesti. Otttieni tutti dati utili per ottimizzare le performance.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row p-4 my-4 text-center gx-4 justify-content-between">
+            <div class="col-3">
+                <div class="rounded-2 border bg-white shadow ratio ratio-1x1">
+                    <div class="box-content">
+                        <div class="number fs-1 mb-2">
+                            {{ $totalApartments }}
+                        </div>
+                        <div class="number-text fw-bold">
+                            Appartamenti inseriti
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="rounded-2 border bg-white shadow ratio ratio-1x1">
+                    <div class="box-content">
+                        <div class="number fs-1  mb-2">
+                            13
+                        </div>
+                        <div class="number-text fw-bold">
+                            Appartamenti sponsorizzati
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="rounded-2 border bg-white shadow ratio ratio-1x1">
+                    <div class="box-content">
+                        <div class="number fs-1  mb-2">
+                            10
+                        </div>
+                        <div class="number-text fw-bold">
+                            Messaggi ricevuti
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="secondary-section py-5">
+    <div class="container">
+        <div class="row">
+            <h2 class="text-center mb-4">Piani di sponsorizzazione</h2>
+            <div class="d-flex flex-wrap">
+                <div class="col-12 col-md-4 text-center mb-4">
+                    <div class="card tier-card bg-1 m-2">
+                        <div class="car-header sponsor-title-1">
+                            standard
+                        </div>
+                        <div class="card-body">
+                            <p class="card-subtitle mb-2 card-price">5.99 €</p>
+                            <p class="card-subtitle mb-2">
+                                Offerta BnB Anonimi della durata di 24 ore
+                            </p>
+                            <p class="card-text mx-2">
+                                Acquistando questo pacchetto il tuo appartamento verra' visualizzato nella pagina principale del nostro sito per una durata di 24 ore, inoltre sara' tra i primi risultati nelle ricerche degli utenti.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4 text-center mb-4">
+                    <div class="card tier-card bg-2 m-2">
+                        <div class="car-header sponsor-title-2">
+                            standard
+                        </div>
+                        <div class="card-body">
+                            <p class="card-subtitle mb-2 card-price">9.99 €</p>
+                            <p class="card-subtitle mb-2">
+                                Offerta BnB Anonimi della durata di 48 ore
+                            </p>
+                            <p class="card-text mx-2">
+                                Acquistando questo pacchetto il tuo appartamento verra' visualizzato nella pagina principale del nostro sito per una durata di 48 ore, inoltre sara' tra i primi risultati nelle ricerche degli utenti.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4 text-center mb-4">
+                    <div class="card tier-card bg-3 m-2">
+                        <div class="car-header sponsor-title-3">
+                            standard
+                        </div>
+                        <div class="card-body">
+                            <p class="card-subtitle mb-2 card-price">12.99 €</p>
+                            <p class="card-subtitle mb-2">
+                                Offerta BnB Anonimi della durata di 144 ore
+                            </p>
+                            <p class="card-text mx-2">
+                                Acquistando questo pacchetto il tuo appartamento verra' visualizzato nella pagina principale del nostro sito per una durata di 144 ore, inoltre sara' tra i primi risultati nelle ricerche degli utenti.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</div>
+        
+        
+        
+        
+            {{-- <div class="rounded-2 border bg-white shadow p-4 my-4">
+            <h3 class="fs-3 fw-bold mb-3">Appartmenti</h3>
+            <div class="row gx-4">
+                
+            </div>
+
+
+
+
+
+
             <div class="w-100 text-end">
                 <a class="btn btn-primary my-3" href="{{ route('user.apartment.index') }}">Vai alla lista degli
                     appartamenti</a>
             </div>
-            <h3 class="fs-3 fw-bold mb-3">Appartmenti</h3>
+            
             @if (!$apartments->isEmpty())
             <div class="row card-body justify-content-center">
                 <div class="col-12">
@@ -96,7 +255,7 @@
         <div>
             <a href="{{ route('user.apartment.create') }}" class="btn btn-primary my-3">Aggiungi nuovo appartamento</a>
         </div>
-        @endif
+        @endif --}}
     </div>
 </div>
 @endsection
