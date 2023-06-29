89199b5f50c27a001022c9005c86d641abe1d822
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-06-29 18:47:41 +0200

    add: save ip for visit

--- app/Http/Controllers/Api/ViewController.php
+++ app/Http/Controllers/Api/ViewController.php
@@ -4,25 +4,55 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\View;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ViewController extends Controller
 {
-    public function views(View $view,Request $request){
-
-        $id_ap=$request['apartment_id'];
-        $visit_date=$request['visit_date'];
-        $ip_address=$request['ip_address'];
+    public function views(Request $request){
         
-        $view->apartment_id=$id_ap;
-        $view->visit_date=$visit_date;
-        $view->ip_address=$ip_address;
-        dd('ciao');
+        $allView = View::where('ip_address', $request['ip_address'])->orderBy('visit_date')->get();
+        $timeRangge = Carbon::now()->subHours(24);
+        $visitForApartment = $allView->whereIn('apartment_id', $request['apartment_id'])->last();
+        
+        if(!$visitForApartment){
+            $id_ap=$request['apartment_id'];
+            $visit_date=$request['visit_date'];
+            $ip_address=$request['ip_address'];
+            
+            $newView = new View();
+            $newView->fill($request->all());
+    
+            $newView->save();
+            return 'salvato';
+        }else{
+            $checkDate = $visitForApartment->visit_date < Carbon::now()->subHours(24);
+            if($checkDate){
+                $id_ap=$request['apartment_id'];
+                $visit_date=$request['visit_date'];
+                $ip_address=$request['ip_address'];
+                
+                $newView = new View();
+                $newView->fill($request->all());
+        
+                $newView->save();
+                return 'controllato e salvato';
+            }
+            return 'solo controllato';
+        }
+        // dd($checkDate);
+        return 'ciao';
+        
+        // $newView->apartment_id=$id_ap;
+        // $newView->visit_date=$visit_date;
+        // $newView->ip_address=$ip_address;
 
-        return response()->json([
-            'success'=>true,
-            'results'=>$view
-        ]);
+// dd($newView);
+
+        // return response()->json([
+        //     'success'=>true,
+        //     'results'=>$view
+        // ]);
        
     }
 }
