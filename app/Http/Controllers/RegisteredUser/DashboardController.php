eb504836a1fe3df5c998c55465891101b37079d7
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-06-29 19:02:18 +0200

    add dd in dash

--- app/Http/Controllers/RegisteredUser/DashboardController.php
+++ app/Http/Controllers/RegisteredUser/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\RegisteredUser;
 
 use App\Http\Controllers\Controller;
 use App\Models\Apartment;
+use App\Models\View;
 use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -14,6 +15,9 @@ class DashboardController extends Controller
     {
         $apartments = Apartment::where('user_id' , '=', Auth::id())->count();
         $apartmentId = Apartment::where('user_id', Auth::id())->select('id')->get();
+        $views = DB::table('views')
+            ->whereIn('apartment_id', $apartmentId);
+        dd($views);
 
         $sponsored = DB::table('apartment_sponsorship')
         ->whereIn('apartment_id', $apartmentId)
